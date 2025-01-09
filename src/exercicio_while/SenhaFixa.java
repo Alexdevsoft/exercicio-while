22896992d1197820fa22d97c5ccbff76ce8a6a79
Alexdevsoft <alexhavilla2022@gmail.com>
2025-01-08 22:34:06 -0300

    2002

--- src/exercicio_while/SenhaFixa.java
+++ src/exercicio_while/SenhaFixa.java
@@ -1,10 +1,24 @@
 package exercicio_while;
 
+import java.util.Scanner;
+
 public class SenhaFixa {
 
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
 		
+		int senha;
+		System.out.print("Digite a senha: ");
+		senha = scanner.nextInt();
+		
+		while(senha != 2002) {
+			System.out.print("Senha inválida! Digite a senha: ");
+			senha = scanner.nextInt();
+		}
+		System.out.println("Acesso permitido!");
+		
+		scanner.close();
 
 	}
 
